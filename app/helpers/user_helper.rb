fa8bc956261dd68a4d744ef77d8db6c57c4804cb
Adam Wead <amsterdamos@gmail.com>
2018-11-29 13:40:40 -0500

    Completed: ability to follow/unfollow users

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,10 @@
 module UserHelper
 
   def follow_button(user)
-    button_to "Follow", follow_user_path
+    if current_user.following?(user)
+      button_to "Unfollow", unfollow_user_path, method: :delete
+    else
+      button_to "Follow", follow_user_path
+    end
   end
 end
