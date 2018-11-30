5d54e8e6836a50c7120114fe192ce20086750978
Adam Wead <amsterdamos@gmail.com>
2018-11-30 11:31:07 -0500

    Searching on hashtags

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -5,4 +5,13 @@ class Shout < ApplicationRecord
   validates :user, presence: true
 
   delegate :username, to: :user
+
+  # Models know a lot about SQL... or use query object
+  # scope :search ->(term) { joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id").where("text_shouts.body LIKE ?", term) }
+
+  # Don't put SQL here
+  # def self.search(term)
+  #   joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id")
+  #     .where("text_shouts.body LIKE ?", term)
+  # end
 end
